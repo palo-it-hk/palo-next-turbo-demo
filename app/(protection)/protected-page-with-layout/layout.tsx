7e059c4019ead355281753486652637dc154fe02
Olevia Yuen <aivelo@icloud.com>
2023-05-10 02:02:50 +0800

    added mobx

--- app/(protection)/protected-page-with-layout/layout.tsx
+++ app/(protection)/protected-page-with-layout/layout.tsx
@@ -1,12 +1,28 @@
-import { ReactNode } from 'react';
+'use client';
 
-export default function ProtectionLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  // get token from store
-  // async validate token with backend
-  // if not valid then
-  // else render children
-}
+import { ReactNode, useEffect, useState } from 'react';
+import { useJwtStore } from 'store/domain-store';
+
+const ProtectionLayout = ({ children }: { children: ReactNode }) => {
+  const [displayContent, setDisplayContent] = useState(false);
+
+  useEffect(() => {
+    authCheck();
+  }, []);
+
+  function authCheck() {
+    const store = useJwtStore();
+    const jwtToken = store.getJwt;
+
+    if (jwtToken === '') {
+      console.log('there is no token');
+      return;
+    }
+    console.log('token is', jwtToken);
+    setDisplayContent(true);
+  }
+
+  return <>{displayContent ? children : <p>Loading...</p>}</>;
+};
+
+export default ProtectionLayout;
---
+++ store/domain-store.ts
@@ -0,0 +1,28 @@
+import { makeAutoObservable } from 'mobx';
+import { makePersistable } from 'mobx-persist-store';
+
+export class JwtStore {
+  jwtToken: string = 'abc';
+
+  constructor() {
+    makeAutoObservable(this);
+    makePersistable(this, {
+      name: 'JwtStore',
+      properties: ['jwtToken'],
+      storage: typeof window === 'undefined' ? undefined : window.localStorage,
+    });
+  }
+
+  get getJwt() {
+    return this.jwtToken;
+  }
+
+  setJwt(jwtToken: string) {
+    this.jwtToken = jwtToken;
+  }
+}
+const jwtStore = new JwtStore();
+
+export function useJwtStore() {
+  return jwtStore;
+}
