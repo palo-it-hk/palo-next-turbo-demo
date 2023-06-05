d1b4beebc8b09628343e071d9340f5a4aa6142e3
Olevia Yuen <aivelo@icloud.com>
2023-06-05 15:04:59 +0800

    added thunk functions

--- components/atomic-design/organisms/AddPostForm/index.tsx
+++ components/atomic-design/organisms/AddPostForm/index.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
-import { postAdded } from 'store/state-management/redux/posts/slice';
 import { useAppDispatch } from 'store/state-management/redux/hook';
+import { addNewPost } from 'store/state-management/redux/posts/slice';
 
-import Subtitle from '../../atoms/Subtitle-TW';
 import PostForm from '../PostForm';
+import Subtitle from '../../atoms/Subtitle-TW';
 
 export const AddPostForm = () => {
   const dispatch = useAppDispatch();
@@ -15,21 +15,16 @@ export const AddPostForm = () => {
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(postAdded(title, content));
+      dispatch(addNewPost({ title, content }));
       setTitle('');
       setContent('');
     }
   };
 
-  const onTitleChanged = useCallback(() => {
-    (e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
-  }, []);
-
-  const onContentChanged = useCallback(
-    () => (e: React.ChangeEvent<HTMLTextAreaElement>) =>
-      setContent(e.target.value),
-    [],
-  );
+  const onTitleChanged = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setTitle(e.target.value);
+  const onContentChanged = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
+    setContent(e.target.value);
 
   return (
     <>
