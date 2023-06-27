58b22fb1b3ec28592ee4cb1371f3c4b835b9892d
Olevia Yuen <aivelo@icloud.com>
2023-06-27 11:30:18 +0800

    added svg implementation

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack"]
+  //   });
+ 
+  //   return config;
+  // },
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
     // Required:
     appDir: true,
   },
+  
 };
 
 module.exports = nextConfig;
