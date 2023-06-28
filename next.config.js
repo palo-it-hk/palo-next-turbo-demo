a6fb3affd8ca751981dd408057977a9e1495a99e
Olevia Yuen <aivelo@icloud.com>
2023-06-28 15:19:21 +0800

    fixed type

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // // Enable to below to use SVGR:
+  // // Enable the below to use SVGR:
   // webpack(config) {
   //   config.module.rules.push({
   //     test: /\.svg$/,
