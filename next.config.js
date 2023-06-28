dad51f59c4f56375cf1007f01cbd5fc15f663145
Olevia Yuen <aivelo@icloud.com>
2023-06-28 15:18:37 +0800

    added comment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // // Enable to below to use SVGR:
   // webpack(config) {
   //   config.module.rules.push({
   //     test: /\.svg$/,
