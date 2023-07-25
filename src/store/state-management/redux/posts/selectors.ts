f4e8a46bd49f75c9db6137b963c5c61fb606f62f
Olevia Yuen <aivelo@icloud.com>
2023-07-25 16:31:55 +0800

    fix selector problem due to using hooks in non react file

--- src/store/state-management/redux/posts/selectors.ts
+++ src/store/state-management/redux/posts/selectors.ts
@@ -2,20 +2,20 @@
 
 import { EntityAdapter } from '@reduxjs/toolkit';
 
-import { Post } from 'store/post.type';
+import { Post } from 'types/post.type';
 
-import { useAppSelector } from '../hook';
+import { useAppSelector as selectFromCurrent } from '../hook';
 import { RootState } from '../store';
 
-export default function (postsAdapter: EntityAdapter<Post>) {
+export default function getSelectors(postsAdapter: EntityAdapter<Post>) {
   // postsAdapter is created with createEntityAdapter(). It enables built-in selectors such as the below
   // For more basic selectors, see https://redux-toolkit.js.org/api/createEntityAdapter#selector-functions
   const defaultSelector = postsAdapter.getSelectors(
     (state: RootState) => state.posts,
   );
 
-  const postStatus = useAppSelector((state) => state.posts.status);
-  const errorStatus = useAppSelector((state) => state.posts.error);
+  const postStatus = selectFromCurrent((state) => state.posts.status);
+  const errorStatus = selectFromCurrent((state) => state.posts.error);
 
   return { ...defaultSelector, postStatus, errorStatus };
 }
