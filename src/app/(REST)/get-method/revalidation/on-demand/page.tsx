75a96346b15690031ff8764cc12d97724889fc6b
Olevia Yuen <aivelo@icloud.com>
2023-08-09 16:52:35 +0800

    correction  to description

--- src/app/(REST)/get-method/revalidation/on-demand/page.tsx
+++ src/app/(REST)/get-method/revalidation/on-demand/page.tsx
@@ -64,13 +64,8 @@ export default async function Page() {
         <p className="font-bold">Whats happening in the background</p>
         <ol className="list-inside list-decimal">
           <li>
-            The button sends a request to &apos;/api/revalidate&apos;, which is
-            an API that revalidates APIs based on which page the request
-            originated from.
-          </li>
-          <li>
-            We refresh the page so that the fetch request for the data is
-            fetched and the page is built again.
+            The buttons sends a request the revalidation APIs. One is
+            revalidated by it&apos;s URL, the other is revalidated by tags.
           </li>
         </ol>
       </div>
