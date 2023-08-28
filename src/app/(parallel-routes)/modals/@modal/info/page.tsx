691a70b465d47b45f193ba58f091c393523c824d
Olevia Yuen <aivelo@icloud.com>
2023-08-28 19:48:11 +0800

    temporarily removed modals

--- src/app/(parallel-routes)/modals/@modal/info/page.tsx
+++
@@ -1,9 +0,0 @@
-import ModalWrapper from 'components/atomic-design/wrappers/ModalWrapper';
-
-export default function Page() {
-  return (
-    <ModalWrapper>
-      <p>More info: this is a good boi.</p>
-    </ModalWrapper>
-  );
-}
